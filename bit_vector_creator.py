fc019176d6f9338970f134a16116a7826000ac83
Jordan Streete <jordan.streete@marleyspoon.com>
2024-05-14 17:21:50 +0200

    add padding and new bit vec printer

--- bit_vector_creator.py
+++ bit_vector_creator.py
@@ -18,6 +18,10 @@ def ascii_to_bit_vectors(ascii_art):
         # Pad the row with zeros to make it the same length as the longest row
         row_copy = row_copy.rjust(longest_vec, '0') if i == 0 else row_copy.ljust(longest_vec, '0')
 
+        # if longest_vec < 64:
+        #     usize_offset = 64 - len(row_copy)
+        #     row_copy = row_copy.center(usize_offset, '0')
+
         bit_vector = int(row_copy, 2)
         print(f"Row: {row},\tBit vector: {bit_vector},\tRow copy: {row_copy}")
 
@@ -79,21 +83,42 @@ def ascii_to_bit_vectors(ascii_art):
 # 1     1  1     111111   1     1     1
 # """
 
+# ascii_art = """\
+#
+#       1   11111   1111111   11111 
+#       1  1           1     1     1
+#       1  1           1     1     1
+#       1  1           1     111111 
+#       1   11111      1     11     
+#       1        1     1     1 1    
+#  1    1        1     1     1  1   
+#  1    1        1     1     1   1  
+#   1111   111111      1     1    1 
+#
+# """
+
 ascii_art = """\
 
-      1   11111   1111111   11111 
-      1  1           1     1     1
-      1  1           1     1     1
-      1  1           1     111111 
-      1   11111      1     11     
-      1        1     1     1 1    
- 1    1        1     1     1  1   
- 1    1        1     1     1   1  
-  1111   111111      1     1    1 
+      1   11111  
+      1  1       
+      1  1       
+      1  1       
+      1   11111  
+      1        1 
+ 1    1        1 
+ 1    1        1 
+  1111   111111  
 
 """
 
+
 bit_vectors, longest_vec = ascii_to_bit_vectors(ascii_art)
+
 # Print the resulting bit vectors
-# longest_vec = len(ascii_art.strip().split('\n')[4])
-print(f"Bit vector: {bit_vectors}, number of lines: {len(bit_vectors)}, number of columns: {longest_vec}")
+# print(f"Bit vector: {bit_vectors}, number of lines: {len(bit_vectors)}, number of columns: {longest_vec}")
+print("Copy and paste this right into the raytracer code:")
+print(f"""\
+const NUM_LINES: usize = {len(bit_vectors)};
+const NUM_COLUMNS: usize = {longest_vec};
+const sphere_positions: [NUM_LINES]usize = .{{ {str(bit_vectors)[1:-1]} }};
+""")
